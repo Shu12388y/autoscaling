061bd849ec75760d4ae92960ebeba4a5b398705b
shu12388y <spl.sp999@gmail.com>
2025-03-02 01:41:02 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const os = require("os");
 const cluster = require("cluster");
+const { exec } = require("child_process");
 const app = express();
 app.use(express.json());
 
@@ -15,13 +16,30 @@ app.get("/cpu", (req, res) => {
   return res.json({ message: "cpu worker" });
 });
 
+app.post("/webhook", async (req, res) => {
+  const data = await req.body;
+  if (!data) {
+    return res.status(408).json({ message: "No data" });
+  }
 
-app.post("/webhook",async(req,res)=>{
-  const {data} = await req.body;
-  console.log(data);
-  return res.status(200).json({message:"Done"})
+  // Kill any process using port 3000 before restarting
+  exec(
+    "ls -al && node -v && git pull &&  pm2 restart index.js",
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    }
+  );
 
-})
+  return res.status(200).json({ message: "Done" });
+});
 
 app.get("/os", (req, res) => {
   return res.json({ message: os.platform() });
